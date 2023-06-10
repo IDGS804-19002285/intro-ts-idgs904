eb7bc3918757f1994b349c6ace8bf90826fec35e
JoseEstrada <jjjestradag@gmail.com>
2023-06-09 20:31:39 -0600

    JJJEG 09/06/2023

---
+++ src/intro-ts/05-clases.ts
@@ -0,0 +1,54 @@
+
+class Persona2{
+    nombre:string='2';
+    edad:number=2;
+    constructor(nombre:string, edad:number){
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+
+    imprimir(){
+        console.log(`Nombre: ${this.nombre} y Edad: ${this.edad}`)
+    }
+
+}
+let persona:Persona2;
+persona=new Persona2('Juan',23)
+persona.imprimir();
+
+
+// Modificadores de acceso 
+
+class Dado{
+    private valor:number=0;
+    public tirar(){
+        this.generar();
+    }
+
+
+    private generar(){
+        this.valor=Math.floor(Math.random()*6)+1
+    }
+
+    public imprimir(){
+        console.log(`Valor: ${this.valor}`)
+    }
+}
+
+let dado1= new Dado();
+dado1.tirar();
+dado1.imprimir();
+
+class Persona3{
+    constructor(public nombre:string, public edad:number){}
+    imprimir(){
+        console.log(`Nombre: ${this.nombre} y Edad: ${this.edad}`)
+    }
+    
+}
+
+let persona3:Persona3;
+persona3= new Persona3('Jose',24);
+persona3.imprimir();
+
+
---
+++ src/intro-ts/07-genericos.ts
@@ -0,0 +1,69 @@
+class PilaEnteros{
+    private vec:number[]=[];
+    insertar(x:number){
+        this.vec.push(x);
+    }
+    extraer(){
+        if(this.vec.length>0)
+            return this.vec.pop();
+        else
+            return null
+    }
+}
+
+class PilaString{
+    private vec:string[]=[];
+    insertar(x:string){
+        this.vec.push(x);
+    }
+    extraer(){
+        if(this.vec.length>0)
+            return this.vec.pop();
+        else
+            return null
+    }
+}
+
+let pila1 = new PilaEnteros();
+pila1.insertar(23);
+pila1.insertar(28);
+pila1.insertar(2);
+console.log(pila1.extraer());
+
+// CADENAS
+let pila2 =new PilaString
+pila2.insertar('23');
+pila2.insertar('28');
+pila2.insertar('2');
+console.log(pila2.extraer());
+
+class PilaGenerica<T>{
+    private vec:T[]=[];
+    insertar(x:T){
+        this.vec.push(x);
+    }
+    extraer(){
+        if(this.vec.length>0)
+            return this.vec.pop();
+        else
+            return null
+    }
+}
+
+let pila3 = new PilaGenerica();
+pila1.insertar(23);
+pila1.insertar(28);
+pila1.insertar(2);
+console.log(pila3.extraer());
+
+// CADENAS
+let pila4 =new PilaGenerica();
+pila2.insertar('23');
+pila2.insertar('28');
+pila2.insertar('2');
+console.log(pila4.extraer());
+
+
+
+
+
